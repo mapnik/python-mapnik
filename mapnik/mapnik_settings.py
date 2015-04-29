15fb1c7741cb26f1a32ad59678864b746c43487f
Blake Thompson <flippmoke@gmail.com>
2015-04-29 15:52:11 -0500

    Made mapnik settings only work in relative path if necessary

--- mapnik/mapnik_settings.py
+++ mapnik/mapnik_settings.py
@@ -1,8 +1,13 @@
 import os
 mapnik_data_dir = os.path.dirname(os.path.realpath(__file__))
-env = {
-    'ICU_DATA': os.path.join(mapnik_data_dir, 'icu'),
-    'GDAL_DATA': os.path.join(mapnik_data_dir, 'gdal'),
-    'PROJ_LIB': os.path.join(mapnik_data_dir, 'proj')
-}
 
+env = {}
+icu_path = os.path.join(mapnik_data_dir, 'icu')
+if os.path.isdir(icu_path):
+    env['ICU_DATA'] = icu_path
+gdal_path = os.path.join(mapnik_data_dir, 'gdal')
+if os.path.isdir(gdal_path):
+    env['GDAL_DATA'] = gdal_path
+proj_path = os.path.join(mapnik_data_dir, 'proj')
+if os.path.isdir(proj_path):
+    env['PROJ_LIB'] = proj_path
