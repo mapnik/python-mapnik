dd2373f35ac63c736f946912539715d55b5e005f
Dane Springmeyer <dane@mapbox.com>
2015-04-30 18:12:54 -0700

    remove parallel compilation hack: this is not ready for primetime

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 #! /usr/bin/env python
 
-# monkey-patch for parallel compilation
-def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
-    # those lines are copied from distutils.ccompiler.CCompiler directly
-    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
-    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
-    # parallel code
-    import multiprocessing
-    import multiprocessing.pool
-    N=multiprocessing.cpu_count() # number of parallel compilations
-    def _single_compile(obj):
-        try: src, ext = build[obj]
-        except KeyError: return
-        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
-    # convert to list, imap is evaluated on-demand
-    try:
-        pool = multiprocessing.pool.ThreadPool(N)
-        list(pool.imap(_single_compile,objects))
-    except KeyboardInterrupt as e:
-        print "Caught KeyboardInterrupt, terminating workers"
-        pool.terminate()
-        raise e
-    return objects
-import distutils.ccompiler
-distutils.ccompiler.CCompiler.compile=parallelCCompile
 from distutils import sysconfig
 from setuptools import setup, Extension
 import os
