f707f760f48e24e84a4a5d5548f92a9c4f231eed
Blake Thompson <flippmoke@gmail.com>
2015-05-27 08:55:21 -0500

    Updated tests and changed bootstrap to point to release candidate 3.

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,7 @@ function install() {
 }
 
 function install_mason_deps() {
-    install mapnik 3.0.0-rc2
+    install mapnik 3.0.0-rc3
     install protobuf 2.6.1
     install freetype 2.5.4
     install harfbuzz 2cd5323
