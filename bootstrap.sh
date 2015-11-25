92e79d27fded02adc96d1d7e65a5e0ea060ecf98
Dane Springmeyer <dane@mapbox.com>
2015-11-25 11:18:28 -0500

    update mason deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,34 +21,33 @@ function install() {
     fi
 }
 
+ICU_VERSION="55.1"
+
 function install_mason_deps() {
     install mapnik latest
-    install protobuf 2.6.1
-    install freetype 2.5.4
-    install harfbuzz 2cd5323
+    install gdal 1.11.2
+    install boost 1.59.0
+    install boost_liball 1.59.0
+    install freetype 2.6
+    install harfbuzz 0.9.40
     install jpeg_turbo 1.4.0
-    install libxml2 2.9.2
-    install libpng 1.6.16
+    install libpng 1.6.17
     install webp 0.4.2
-    install icu 54.1
+    install icu ${ICU_VERSION}
     install proj 4.8.0
     install libtiff 4.0.4beta
-    install boost 1.57.0
-    install boost_libsystem 1.57.0
-    install boost_libthread 1.57.0
-    install boost_libfilesystem 1.57.0
-    install boost_libprogram_options 1.57.0
-    install boost_libpython 1.57.0
-    install boost_libregex 1.57.0
-    install boost_libpython 1.57.0
+    install libpq 9.4.0
+    install sqlite 3.8.8.1
+    install expat 2.1.0
     install pixman 0.32.6
-    install cairo 1.12.18
+    install cairo 1.14.2
+    install protobuf 2.6.1
 }
 
 function setup_runtime_settings() {
     local MASON_LINKED_ABS=$(pwd)/mason_packages/.link
     export PROJ_LIB=${MASON_LINKED_ABS}/share/proj
-    export ICU_DATA=${MASON_LINKED_ABS}/share/icu/54.1
+    export ICU_DATA=${MASON_LINKED_ABS}/share/icu/${ICU_VERSION}
     export GDAL_DATA=${MASON_LINKED_ABS}/share/gdal
     if [[ $(uname -s) == 'Darwin' ]]; then
         export DYLD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${DYLD_LIBRARY_PATH}
