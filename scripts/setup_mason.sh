f2dca360561bc24cee0b2826f40a79366b2e1a77
Dane Springmeyer <dane@mapbox.com>
2016-08-30 11:51:18 -0700

    Get correct mason version

--- scripts/setup_mason.sh
+++ scripts/setup_mason.sh
@@ -4,7 +4,7 @@ set -eu
 set -o pipefail
 
 # we pin the mason version to avoid changes in mason breaking builds
-MASON_VERSION="3ddc419"
+MASON_VERSION="new-pkgs"
 
 function setup_mason() {
     if [[ ! -d ./.mason ]]; then
