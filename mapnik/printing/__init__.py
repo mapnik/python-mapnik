mapnik/python-mapnik

commit 4f7f090e1f24a8846a2bdb521fe4569a1b0c07e5

An attempt to solve printing import error

mapnik/printing/__init__.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import, print_function
 import logging
 import math
 
-import mapnik
 from mapnik import Box2d, Coord, Geometry, Layer, Map, Projection, Style, render
 from mapnik.printing.conversions import m2pt, m2px
 from mapnik.printing.formats import pagesizes
@@ -980,6 +979,8 @@ class PDFPrinter(object):
 
     def _create_legend_item_map(self, m, layer, f, legend_map_size):
         """Creates the legend map, i.e., a minified version of the layer map, and returns it."""
+        from mapnik import MemoryDatasource
+
         legend_map = Map(legend_map_size[0], legend_map_size[1], srs=m.srs)
 
         # the buffer is needed to ensure that text labels that overflow the edge of the
@@ -989,7 +990,7 @@ class PDFPrinter(object):
             lestyle = self._get_layer_style_valid_rules(m, layer_style)
             legend_map.append_style(layer_style, lestyle)
 
-        ds = mapnik.MemoryDatasource()
+        ds = MemoryDatasource()
         if f is None:
             ds = layer.datasource
             layer_srs = layer.srs

mapnik/__init__.py
@@ -73,9 +73,6 @@ bootstrap_env()
 
 from ._mapnik import *
 
-from . import printing
-printing.renderer = render
-
 # The base Boost.Python class
 BoostPythonMetaclass = Coord.__class__
 

commit 213675a28a941c6f865d4cd8170a8e562d01a9fb

printing: Update documentation and test
Now that printing isn't loaded automatically
it must be loaded explicitly.

Fixes: 4f7f090e An attempt to solve printing import error
Signed-off-by: Asbjørn Sloth Tønnesen <asbjorn@asbjorn.st>

mapnik/printing/__init__.py
@@ -65,6 +65,7 @@ class PDFPrinter(object):
     Main class for creating PDF print outs. Basic usage is along the lines of
 
     import mapnik
+    import mapnik.printing
 
     page = mapnik.printing.PDFPrinter()
     m = mapnik.Map(100,100)

test/python_tests/pdf_printing_test.py
@@ -31,6 +31,8 @@ def make_pdf(m, output_pdf, esri_wkt):
 	page.add_geospatial_pdf_header(m, output_pdf, wkt=esri_wkt)
 
 if mapnik.has_pycairo():
+	import mapnik.printing
+
 	def test_pdf_printing():
 		source_xml = '../data/good_maps/marker-text-line.xml'.encode('utf-8')
 		m = make_map_from_xml(source_xml)