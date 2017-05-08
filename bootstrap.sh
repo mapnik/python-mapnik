e8d6ca4af6c0d55445548233cb70c199f7f98d7b
artemp <artem@mapnik.org>
2017-05-08 10:02:17 +0200

    avoid unbound variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,9 +47,9 @@ function setup_runtime_settings() {
     export ICU_DATA=${MASON_LINKED_ABS}/share/icu/${ICU_VERSION}
     export GDAL_DATA=${MASON_LINKED_ABS}/share/gdal
     if [[ $(uname -s) == 'Darwin' ]]; then
-        export DYLD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${DYLD_LIBRARY_PATH}
+        export DYLD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${DYLD_LIBRARY_PATH-}
     else
-        export LD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${LD_LIBRARY_PATH}
+        export LD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${LD_LIBRARY_PATH-}
     fi
     export PATH=$(pwd)/mason_packages/.link/bin:${PATH}
 }
