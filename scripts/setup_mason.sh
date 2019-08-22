a66006ecdcf0686c0bc937e90dd42a9320c3176e
Mickey Rose <mickey.rose@seznam.cz>
2019-08-22 12:24:47 +0200

    add submodule mason/ and pin to v0.20.0

--- scripts/setup_mason.sh
+++ scripts/setup_mason.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-set -eu
-set -o pipefail
-
 # we pin the mason version to avoid changes in mason breaking builds
-MASON_VERSION="3870d9c"
+MASON_ARCHIVE_VERSION="v0.20.0"
+MASON_ARCHIVE_BASE="https://github.com/mapbox/mason/archive"
 
 function setup_mason() {
-    mkdir -p ./mason
-    curl -sSfL https://github.com/mapbox/mason/archive/${MASON_VERSION}.tar.gz | tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=./mason
-    export MASON_HOME=$(pwd)/mason_packages/.link
-    export PATH=$(pwd)/mason:${PATH}
+    local cdup=
+    if cdup=$(git rev-parse --show-cdup 2>/dev/null); then
+        # we are inside *some* git repository (not necessarily python-mapnik)
+        if git submodule status "${cdup}mason" >/dev/null 2>&1; then
+            # there is submodule "mason" (assume we are in python-mapnik)
+            # update the submodule, bail out on failure
+            git submodule update --init "${cdup}mason" ||
+                return
+        else
+            # there is no submodule named "mason"
+            # proceed as if we were outside git repository
+            cdup=
+        fi
+    fi
+    if [ ! -d "${cdup}mason/" ]; then
+        # the directory doesn't exist, and we are either outside any git
+        # repository, or in a repository with no submodule named "mason"
+        mkdir -p "${cdup}mason/"
+        # download and unpack mason archive
+        curl -sSfL "${MASON_ARCHIVE_BASE}/${MASON_ARCHIVE_VERSION}.tar.gz" |
+            tar --extract --gunzip --directory="${cdup}mason/" \
+                --strip-components=1 --exclude="test" ||
+            return
+    fi
+    export PATH=$(cd "${cdup}mason" && pwd):${PATH}
     export CXX=${CXX:-clang++}
     export CC=${CC:-clang}
 }
 
 
 setup_mason
-
-set +eu
-set +o pipefail
