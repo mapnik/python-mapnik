df0a672b537f712363f014a30c00fbc83785177b
Artem Pavlenko <artem@mapnik.org>
2024-04-29 14:48:29 +0100

    New mapnik Python bindings using pybind11 (https://github.com/pybind/pybind11)  [WIP] [skip ci]

--- src/mapnik_coord.cpp
+++ src/mapnik_coord.cpp
@@ -2,7 +2,7 @@
  *
  * This file is part of Mapnik (c++ mapping toolkit)
  *
- * Copyright (C) 2015 Artem Pavlenko, Jean-Francois Doyon
+ * Copyright (C) 2024 Artem Pavlenko
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,50 +19,46 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *****************************************************************************/
-#include <mapnik/config.hpp>
-
-
-#pragma GCC diagnostic push
-#include <mapnik/warning_ignore.hpp>
-#include <boost/python.hpp>
-#pragma GCC diagnostic pop
 
 // mapnik
+#include <mapnik/config.hpp>
 #include <mapnik/coord.hpp>
+//pybind11
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
 
+namespace py = pybind11;
 using mapnik::coord;
 
-struct coord_pickle_suite : boost::python::pickle_suite
+void export_coord(py::module const& m)
 {
-    static boost::python::tuple
-    getinitargs(const coord<double,2>& c)
-    {
-        using namespace boost::python;
-        return boost::python::make_tuple(c.x,c.y);
-    }
-};
-
-void export_coord()
-{
-    using namespace boost::python;
-    class_<coord<double,2> >("Coord",init<double, double>(
-                                 // class docstring is in mapnik/__init__.py, class _Coord
-                                 (arg("x"), arg("y")),
-                                 "Constructs a new point with the given coordinates.\n")
-        )
-        .def_pickle(coord_pickle_suite())
+    py::class_<coord<double,2> >(m, "Coord")
+        .def(py::init<double, double>(),
+             // class docstring is in mapnik/__init__.py, class _Coord
+             "Constructs a new object with the given coordinates.\n",
+             py::arg("x"), py::arg("y"))
         .def_readwrite("x", &coord<double,2>::x,
                        "Gets or sets the x/lon coordinate of the point.\n")
         .def_readwrite("y", &coord<double,2>::y,
                        "Gets or sets the y/lat coordinate of the point.\n")
-        .def(self == self) // __eq__
-        .def(self + self) // __add__
-        .def(self + float())
-        .def(float() + self)
-        .def(self - self) // __sub__
-        .def(self - float())
-        .def(self * float()) //__mult__
-        .def(float() * self)
-        .def(self / float()) // __div__
+        .def(py::self == py::self) // __eq__
+        .def(py::self + py::self)  //__add__
+        .def(py::self + float())
+        .def(float() + py::self)
+        .def(py::self - py::self)  //__sub__
+        .def(py::self - float())
+        .def(py::self * float())   //__mult__
+        .def(float() * py::self)
+        .def(py::self / float())   // __div__
+        .def(py::pickle(
+                 [](coord<double,2> & c) {
+                     return py::make_tuple(c.x, c.y);
+                 },
+                 [](py::tuple t) {
+                     if (t.size() != 2)
+                         throw std::runtime_error("Invalid state");
+                     coord<double,2> c{t[0].cast<double>(),t[1].cast<double>()};
+                     return c;
+                 }))
         ;
 }
